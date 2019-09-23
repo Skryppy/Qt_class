67ec11895ad8f110bcf3e6b87615da400e9d3727
Skryppy <38378802+Skryppy@users.noreply.github.com>
2019-09-23 20:49:41 +0300

    Qt QObject my_class (not work)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
-#include "mainwindow.h"
-#include "my_class.h"
-
+//#include "mainwindow.h"
 
 #include <QApplication>
+
+#include <QObject>
+#include <QtWidgets>
+
+#include "testclass.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-//    My_class my_class;
-//    connect(this, SIGNAL(sendData(QString)), my_class, SLOT(recieveData(QString)));
+//    MainWindow w;
+//    w.show();
+//
 
+    QPushButton btn("Click Me");
+    TestClass test;
+    QObject::connect(&btn, SIGNAL(clicked(bool)), &test, SLOT(slotRecieveData()));
 
+    btn.show();
 
     return a.exec();
-
-
-
-
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,14 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include "my_class.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
-
-//namespace Ui { class My_class; }
-
-
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -22,21 +17,5 @@ public:
 
 private:
     Ui::MainWindow *ui;
-
-//    Ui::My_class *my_class;
-
-//    Ui2::My_class *my_class;
-
-    My_class *my_class;
-
-signals:
-    sendData(QString str);
-
-private slots:
-    void onButtonSend();
-
-//
-//    My_class *my_class = new My_class;
-    void on_pushButton_2_clicked();
 };
 #endif // MAINWINDOW_H
